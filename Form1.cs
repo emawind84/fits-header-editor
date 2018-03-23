882d61b1d302a0d43dcbdbbde8affea9c7c5802b
Emanuele Disco <emawind84@gmail.com>
2018-03-23 18:13:36 +0900

    update

--- Form1.cs
+++ Form1.cs
@@ -476,7 +476,7 @@ namespace FitsHeaderEditor
             try
             {
                 var obj = new HeaderField("", ""); addHeaderField(obj);
-                obj = new HeaderField("", "      / Added with FitsHeaderEditor"); addHeaderField(obj);
+                obj = new HeaderField("", "/ Added with FitsHeaderEditor"); addHeaderField(obj);
                 obj = new HeaderField("OBJECT", Properties.Settings.Default.HeaderFieldObject); addHeaderField(obj);
                 obj = new HeaderField("TELESCOP", Properties.Settings.Default.HeaderFieldTelescope); addHeaderField(obj);
                 obj = new HeaderField("INSTRUME", Properties.Settings.Default.HeaderFieldInstrument); addHeaderField(obj);
